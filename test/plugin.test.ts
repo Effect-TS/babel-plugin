87c1255b3e727ef3f57511682aa606e484137e75
Maxwell Brown <maxwellbrown1990@gmail.com>
2023-01-28 19:54:41 -0500

    migrate to new project structure

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -1,4 +1,4 @@
-import plugin from "@repo-tooling/babel-plugin-fp-ts"
+import plugin from "@repo-tooling/babel-plugin"
 import pluginTester from "babel-plugin-tester"
 
 pluginTester({
@@ -7,74 +7,101 @@ pluginTester({
   tests: {
     "should re-write named pipe imports": {
       code: `
-        import { pipe } from "@fp-ts/data/Function"
+        import { pipe } from "@fp-ts/core/Function"
         const test = pipe(a, map(() => b))
       `,
       output: "const test = map(() => b)(a);"
     },
     "should re-write namespaced pipe imports": {
       code: `
-        import * as F from "@fp-ts/data/Function"
+        import * as F from "@fp-ts/core/Function"
         const test = F.pipe(a, map(() => b))
       `,
       output: "const test = map(() => b)(a);"
     },
     "should not remove named imports if they are still referenced": {
       code: `
-        import { pipe, double } from "@fp-ts/data/Function"
+        import { pipe, double } from "@fp-ts/core/Function"
         const test = pipe(a, double)
       `,
       output: `
-        import { double } from "@fp-ts/data/Function";
+        import { double } from "@fp-ts/core/Function";
         const test = double(a);
       `
     },
     "should not remove namespaced imports if they are still referenced": {
       code: `
-        import * as F from "@fp-ts/data/Function"
+        import * as F from "@fp-ts/core/Function"
         const test = F.pipe(a, F.double)
       `,
       output: `
-        import * as F from "@fp-ts/data/Function";
+        import * as F from "@fp-ts/core/Function";
         const test = F.double(a);
       `
     },
     "should re-write named identity imports": {
       code: `
-        import { identity } from "@fp-ts/data/Function"
+        import { identity } from "@fp-ts/core/Function"
         const test = identity(a)
       `,
       output: `const test = a;`
     },
     "should re-write namespaced identity imports": {
       code: `
-        import * as F from "@fp-ts/data/Function"
+        import * as F from "@fp-ts/core/Function"
         const test = F.identity(a)
       `,
       output: `const test = a;`
     },
     "should re-write identity when used within a pipe": {
       code: `
-        import { identity, pipe } from "@fp-ts/data/Function"
+        import { identity, pipe } from "@fp-ts/core/Function"
         const test = pipe(a, identity, f(() => b))
       `,
       output: `const test = f(() => b)(a);`
     },
     "should allow a user to override targeted imports": {
       pluginOptions: {
-        pipeImports: ["foo"],
-        identityImports: ["bar"]
+        rewritePipe: {
+          targetImports: ["foo"]
+        },
+        rewriteIdentity: {
+          targetImports: ["bar"]
+        }
       },
       code: `
-        import { pipe } from "@fp-ts/data/Function"
+        import { pipe } from "foo"
+        const test = pipe(a, map(() => b))
+      `,
+      output: "const test = map(() => b)(a);"
+    },
+    "should disable rewriting pipe": {
+      pluginOptions: {
+        rewritePipe: false
+      },
+      code: `
+        import { pipe } from "@fp-ts/core/Function"
         const test = pipe(a, map(() => b))
       `,
       output: `
-        import { pipe } from "@fp-ts/data/Function";
+        import { pipe } from "@fp-ts/core/Function";
         const test = pipe(
           a,
           map(() => b)
         );
+      `
+    },
+    "should disable rewriting identity": {
+      pluginOptions: {
+        rewriteIdentity: false
+      },
+      code: `
+        import { identity } from "@fp-ts/core/Function"
+        const test = identity(a)
+      `,
+      output: `
+        import { identity } from "@fp-ts/core/Function";
+        const test = identity(a);
       `
     }
   }
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,14 +4,14 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   test: {
-    include: ["packages/*/test/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    exclude: ["packages/*/test/**/util.ts"],
+    include: ["./test/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: ["./test/utils/**/*.ts", "./test/**/*.init.ts"],
     globals: true
   },
   resolve: {
     alias: {
-      "@repo-tooling/babel-plugin-fp-ts/test": path.resolve(__dirname, "packages/babel-plugin-fp-ts/test"),
-      "@repo-tooling/babel-plugin-fp-ts": path.resolve(__dirname, "packages/babel-plugin-fp-ts/src")
+      "@repo-tooling/babel-plugin/test": path.join(__dirname, "test"),
+      "@repo-tooling/babel-plugin": path.join(__dirname, "src")
     }
   }
 })
